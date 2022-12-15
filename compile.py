f0ecafcef6465e0b241c64da7a0b07de16fd1f10
Matt Maginniss <maginniss.matt@gmail.com>
2022-12-16 00:20:03 +0100

    Added script to compile data and add IDs

---
+++ compile.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+result_data = pd.read_csv("./results.csv")
+goalscorer_data = pd.read_csv("./goalscorers.csv")
+shootout_data = pd.read_csv("./shootouts.csv")
+
+# Merge create result ID
+result_data['home_score'] = result_data['home_score'].fillna(0).astype(int)
+result_data['away_score'] = result_data['away_score'].fillna(0).astype(int)
+result_data.insert(0, 'result_id', range(0, len(result_data)))
+result_data.to_csv("./compiled_data/results.csv", index=False)
+
+# Merge Result ID to goalscorer data
+goalscorer_data = pd.merge(goalscorer_data, result_data[['result_id', 'home_team', 'away_team', 'date']], on=['home_team', 'away_team', 'date'], how='left')
+goalscorer_data['minute'] = goalscorer_data['minute'].fillna(0).astype(int)
+goalscorer_column_titles = ['result_id', 'date', 'home_team', 'away_team', 'team', 'scorer', 'minute', 'own_goal', 'penalty']
+goalscorer_data = goalscorer_data.reindex(columns=goalscorer_column_titles)
+goalscorer_data.insert(0, 'goal_id', range(0, len(goalscorer_data)))
+goalscorer_data.to_csv("./compiled_data/goalscorers.csv", index=False)
+
+# Merge Result ID to shootout data
+shootout_data = pd.merge(shootout_data, result_data[['result_id', 'home_team', 'away_team', 'date']], on=['home_team', 'away_team', 'date'], how='left')
+shootout_column_titles = ['result_id', 'date', 'home_team', 'away_team', 'winner']
+shootout_data = shootout_data.reindex(columns=shootout_column_titles)
+shootout_data['result_id'] = shootout_data['result_id'].fillna(0).astype(int)
+shootout_data.insert(0, 'shootout_id', range(0, len(shootout_data)))
+shootout_data.to_csv("./compiled_data/shootouts.csv", index=False)
